1d641cabcd9ad00bd41d61a3fa98087853b6b770
zorell11 <zorell@azet.sk>
2023-12-01 10:57:36 +0100

    add day1

--- 01/01.py
+++ 01/01.py
@@ -1,16 +1,14 @@
 with open('input', 'r') as f:
     data = f.read().splitlines()
 
-print(data)
 
-
-sum = 0
+sum_values = 0
 
 for line in data:
     nums = []
     for i in line:
         if i.isnumeric():
             nums.append(i)
-    sum += int(f'{nums[0]}{nums[-1]}')
+    sum_values += int(nums[0] + nums[-1])
 
-print(sum)
+print(sum_values)
--- 01/02.py
+++ 01/02.py
@@ -4,18 +4,18 @@ with open('input', 'r') as f:
 
 numbers = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
 
-sum = 0
+sum_values = 0
 
 for line in data:
     digits = []
     for index,sign in enumerate(line):
         if sign.isnumeric():
-            digits.append(int(sign))
+            digits.append(sign)
         else:
             for num in numbers:
                 length = len(num)
                 if num.startswith(sign) and (line[index:index+length] == num):
                     digits.append(numbers[num])
-    sum += int(f'{digits[0]}{digits[-1]}')
+    sum_values += int(str(digits[0]) + str(digits[-1]))
 
-print(sum)
+print(sum_values)
